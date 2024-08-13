bc09e714bc946b0ab4084bf6b6d15f4fa9b75ff0
Eduardo-RFarias <eduardo.rfarias@outlook.com>
2024-08-13 10:20:58 -0300

    Esqueleto Backend

---
+++ code/backend/src/test/kotlin/br/unb/unbreja/UnbrejaApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.unb.unbreja
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UnbrejaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
